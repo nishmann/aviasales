18870f705133d16f616d6ca049e283086ec28fe6
nischmann <aziretsuiuntbek@gmail.com>
2022-07-31 19:05:13 +0600

    white loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import style from './Loader.module.scss';
 
 const Loader: React.FC = () => {
-  const antIcon = <LoadingOutlined style={{ fontSize: 62 }} spin />;
+  const antIcon = <LoadingOutlined style={{ fontSize: 62, color: '#fff' }} spin />;
   return (
     <div className={style.loader}>
       <Spin indicator={antIcon} />
